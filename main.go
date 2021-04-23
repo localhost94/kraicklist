4b9d58cb70d8c7a171cb70ed2d235faea71e29de
arya <arya@badr-interactive.com>
2021-04-24 06:18:40 +0700

    add .env

--- main.go
+++ main.go
@@ -83,7 +83,10 @@ func LoadDatabase() http.HandlerFunc {
 				return
 			}
 
-			uri := "mongodb://localhost:27017"
+			uri := os.Getenv("MONGO_URL")
+			if port == "" {
+				uri = "mongodb://localhost:27017"
+			}
 			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 			defer cancel()
 			client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
