f88cc8f782e2f474267766e19c6fe6a73cd65f6f
arya <arya@badr-interactive.com>
2021-04-25 22:06:45 +0700

    update auth

--- main.go
+++ main.go
@@ -76,11 +76,14 @@ func LoadDatabase() http.HandlerFunc {
 				w.Write([]byte("missing search query in query params"))
 				return
 			}
-
-			uri := "mongodb+srv://root:bismillah@mongo/kraicklist?retryWrites=true&w=majority"
+			credential := options.Credential{
+					Username: "root",
+					Password: "bismillah",
+			}
+			uri := "mongodb://mongo:27017"
 			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 			defer cancel()
-			client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+			client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri)).SetAuth(credential)
 			if err != nil { log.Fatal(err) }
 
 			collection := client.Database("kraicklist").Collection("data")
